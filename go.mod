40c30f776daabbf725ec6df8325059972974edb7
nishant <nishantpriya.scorpion@gmail.com>
2026-03-27 18:58:32 +0530

    server

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/nishantxoxo/rssag
 
 go 1.26.1
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/cors v1.2.2
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello")
+	// fmt.Println("hello")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("port empty")
+	}
+
+
+	router:= chi.NewRouter()
+	router.Use(cors.Handler(
+		cors.Options{
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"*"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300},
+	),
+	)
+
+
+
+	serv := &http.Server{
+		Handler: router,
+		Addr: ":"+portString,
+	}
+
+
+	log.Printf("Server startiung on Port %v", portString)
+ 	err :=	serv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+
+	// fmt.Println("PORT- ", portString)
 }
